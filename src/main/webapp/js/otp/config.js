976907262853d8bf1121a05de3e9a42c5f513dcb
Kevin Webb <kpwebb@gmail.com>
2012-05-11 14:11:01 -0400

    changed map base layer to mb streets

--- src/main/webapp/js/otp/config.js
+++ src/main/webapp/js/otp/config.js
@@ -7,32 +7,18 @@ if(typeof(otp.config) == "undefined" || otp.config == null) otp.config = {};
 // step 2: create an object of default otp.config default values (see step3 where we apply this to any existing config)
 otp.config_defaults = {
 
-    hostname : "http://localhost:8080",
+    hostname : "http://dc-bike.deployer.opentripplanner.org",
     
     // default cloudmade tiles:
-    // tileUrl : 'http://{s}.tile.cloudmade.com/882f51841e1f47e9b81b3e258e6d76b4/997/256/{z}/{x}/{y}.png',
+    // tileUrl : 'http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streetis/{z}/{x}/{y}.png',
     // tileAttrib : 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
 
     // devseed tiles (DC only) & tile URL function override:
-    tileUrl : 'http://{s}.tiles.mapbox.com/devseed/1.0.0/devseed-hq/{z}/{x}/{y}.png',
+    tileUrl : 'http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png',
     tileAttrib : 'Map tiles &copy; Development Seed, Routing powered by OpenTripPlanner',
-
-  	getTileUrl : function(tilePoint, zoom){
-	    var subdomains = this.options.subdomains,
-		    s = this.options.subdomains[(tilePoint.x + tilePoint.y) % subdomains.length];
-
-	    return_url = this._url
-		    .replace('{s}', s)
-		    .replace('{z}', zoom)
-		    .replace('{x}', tilePoint.x)
-		    .replace('{y}', Math.pow(2,zoom) - tilePoint.y -1);
-	    //console.debug("url = " + return_url + " & x, y, z = " + tilePoint.x+","+tilePoint.y+","+zoom)
-	    return return_url;
-    },
-	        
-    
     initLatLng : new L.LatLng(38.89355, -77.0146), // DC
     initZoom : 14,
+    maxZoom : 17,
 
     CLASS_NAME : "otp.config"
 };
