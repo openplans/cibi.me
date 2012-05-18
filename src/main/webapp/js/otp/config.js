cc05352e93dad84c039141cd36202e0b3bb04140
Kevin Webb <kpwebb@gmail.com>
2012-05-18 02:56:33 -0400

    changed map credits

--- src/main/webapp/js/otp/config.js
+++ src/main/webapp/js/otp/config.js
@@ -16,7 +16,7 @@ otp.config = {
     // devseed tiles (DC only) & tile URL function override:
     tileUrl : 'http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-light/{z}/{x}/{y}.png',
     overlayTileUrl : 'http://{s}.tiles.mapbox.com/v3/intertwine.nyc_bike_overlay/{z}/{x}/{y}.png',
-    tileAttrib : 'Routing powered by <a href="http://opentripplanner.org/">OpenTripPlanner</a>, Map tiles &copy; Development Seed and OpenStreetMap ',
+    tileAttrib : 'Routing powered by <a href="http://opentripplanner.org/">OpenTripPlanner</a>, Map tiles from MapBox (<a href="http://mapbox.com/about/maps/">terms</a>) and OpenStreetMap ',
     initLatLng : new L.LatLng(40.719298,-73.999743), // NYC
     initZoom : 14,
     minZoom : 13,
